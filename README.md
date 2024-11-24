c701799fa5a17a3fc0673b1294cdc2ff371e824c
aimwizards <artisticwonder123@gmail.com>
2024-11-24 23:33:41 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# sb1-zp6hgp
+# nexxuurra
 
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/berlinmoneie/sb1-zp6hgp)
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/berlinmoneie/nexxuurra)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      container: {
-        center: true,
-        padding: '1rem',
-      },
-      animation: {
-        'spin-slow': 'spin 3s linear infinite',
-        'bounce-slow': 'bounce 3s infinite',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
